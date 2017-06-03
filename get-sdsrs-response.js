babf00e9568a939b4a8e3c4ee1a394093acb56fd
Austin Almond <austin226@live.com>
2017-06-03 17:07:28 -0400

    Display response text

--- get-sdsrs-response.js
+++ get-sdsrs-response.js
@@ -1,12 +1,15 @@
 function getServerResponse(inputText) {
-    var userInput = $('#results').text();
+    console.log('Processing - "' + inputText + '"');
     // TODO send user input off to server
 
     var responseText = "This is a sample response. Yo yo yo!";
+
+    console.log('Received response - "' + responseText + '"');
     return responseText;
 }
 
 function actOnSpeechRecognitionResult(text) {
     var response = getServerResponse(text);
-    console.log(response);
+
+    $('#response_from_sdsrs').text(response);
 }
